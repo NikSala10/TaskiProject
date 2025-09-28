cecccc2461b28084c5ff3c93a922b0bccbdf803d
yeliani <yelianibarbosa@gmail.com>
2025-09-28 13:27:11 -0500

    feat(app):creating routes for groups

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Login/Login";
 import ToDoBuild from "./pages/Register/Register";
 import FormLogin from "./components/FormLogin/FormLogin";
+import Register from "./components/FormRegister/FormRegister";
+import CreateGroup from "./pages/CreateGroup/CreateGroup";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/build" element={<ToDoBuild />} />
-        <Route path="/Register" element={<FormRegister />} />
-        <Route path="/create-group" element={<CreateGroupPage />} />
+        <Route path="/Register" element={<Register />}/>
+        <Route path="/create-group" element={<CreateGroup/>}/>
         <Route path="/Login" element={<FormLogin />} />
 
 
@@ -19,5 +21,4 @@ function App() {
     </Router>
   );
 }
-
 export default App;
