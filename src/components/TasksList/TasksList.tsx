4dd50a0d1deddcb566808818f1961f4a8a0136f3
NikSala10 <salamancanikol10@gmail.com>
2025-09-28 16:31:38 -0500

    style(components/taskList): add styles for TaskList component

--- src/components/TasksList/TasksList.tsx
+++ src/components/TasksList/TasksList.tsx
@@ -3,13 +3,13 @@ import TaskItem from "../TaskItem/TaskItem";
 
 const TasksList = ({ tasks }: TaskListProps) => {
   return (
-    <div className="tasks-list" style={{ display: "flex", gap: "20px" }}>
+    <div className="tasks-list">
       <div className="tasks-column">
         {tasks.slice(0, 4).map((task) => (
           <TaskItem key={task.id} task={task} />
         ))}
       </div>
-      
+
       <div className="tasks-column">
         {tasks.slice(4).map((task) => (
           <TaskItem key={task.id} task={task} />
