fa065bd722880a786efafddec68afec08bcfd1fd
yeliani <yelianibarbosa@gmail.com>
2025-09-27 19:01:46 -0500

    loginpage

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,21 @@
 
-const Login = () => {
+import React from "react";
+import LoginForm from "../../components/FormLogin/FormLogin";
+
+const LoginPage: React.FC = () => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Aquí irá la lógica de login
+    console.log("Login submitted");
+  };
 
   return (
-    <>
-      
-    </>
+    <div className="login-page">
+      <div className="login-container">
+        <LoginForm onSubmit={handleSubmit} />
+      </div>
+    </div>
   );
 };
 
-export default Login;
+export default LoginPage;
