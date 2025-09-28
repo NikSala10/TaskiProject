9d2a71006fbb08f43b328afbc19ab6417acf7bdd
SilvanaCuartasS <silvanacuartassilva@gmail.com>
2025-09-28 16:46:53 -0500

    feat(component card): Addition of new base types for the card

--- src/pages/Plan Review/PlanReview.tsx
+++ src/pages/Plan Review/PlanReview.tsx
@@ -1,16 +1,14 @@
 import { useSetPageInfo } from "../../hook/UseSetPage";
-import PlanCard from "../../components/CardPlanReview/Card";
+import Card from "../../components/CardPlanReview/Card";
 
 const PlanReview = () => {
-    
-    useSetPageInfo("Plan Review");
+  useSetPageInfo("Plan Review");
+  
   return (
-     <>
-     <p>Plan Review</p>
-     <PlanCard />
-     <PlanCard />
-     </>
-    
+    <>
+    <Card />
+    </>
+  
   );
 };
 
--- src/types/PlanReview.tsx
+++ src/types/PlanReview.tsx
@@ -1,7 +1,7 @@
+// Tipos para el período de facturación
+export type PeriodType = "Monthly" | "Semi-Annual" | "Annual";
 
-// La interfaz de TypeScript para las props del componente
-export interface PlanCardProps {
-  familyName?: string; //Renombra la card
-  initialBudget?: number; //presupuesto inicial
+
+export interface CardProps {
+
 }
-
