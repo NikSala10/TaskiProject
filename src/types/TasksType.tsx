f6402e569e21937b016c6fc08820836470f49896
NikSala10 <salamancanikol10@gmail.com>
2025-09-28 14:35:20 -0500

    fix(components/cardItem): add new name of the component, taskItem

--- src/types/TasksType.tsx
+++ src/types/TasksType.tsx
@@ -1,5 +1,3 @@
-// src/types/TasksType.ts
-
 export type Task = {
   id: string;
   name: string;
@@ -8,12 +6,10 @@ export type Task = {
   priority: 'Low' | 'Medium' | 'High';
 };
 
-// Para TaskItem, se pasa un solo task.
 export type TaskItemProps = {
   task: Task;
 };
 
-// Para TasksList, se pasa un array de tasks.
 export type TaskListProps = {
   tasks: Task[];
 };
