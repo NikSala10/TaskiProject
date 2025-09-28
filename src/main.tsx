111f413f934443c244b375ac23fe1002b8b6fa72
NikSala10 <salamancanikol10@gmail.com>
2025-09-28 13:33:12 -0500

    feat(pages/tasks): ads btn and complete css, add additional tasks texts

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ToDoProvider } from "./context/ToDoContext.tsx";
 import { PageProvider } from "./context/PageContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ToDoProvider>
-      <PageProvider>
-        <App />
-      </PageProvider>
-    </ToDoProvider>
+    <PageProvider>
+      <App />
+    </PageProvider>
   </StrictMode>
 );
--- src/pages/Tasks/Tasks.tsx
+++ src/pages/Tasks/Tasks.tsx
@@ -1,10 +1,24 @@
+import Button from "../../components/Button/Button";
 import { useSetPageInfo } from "../../hook/UseSetPage";
+import "./Tasks.css";
 
 const Tasks = () => {
- 
-    useSetPageInfo("Tasks");
+  useSetPageInfo("Tasks");
+
   return (
-    <p>Tasks</p>
+    <div className="tasks-page">
+      <div className="btn-create-task">
+        <Button text="Create Task" color="#82C2F6" width="180px" />
+      </div>
+      <h3 className="tit">All</h3>
+      <div className="aditional-info">
+        <h3>Additional tasks</h3>
+        <div className="tetxt">
+          <p>You can accept 3 additional tasks and review them</p>
+          <Button text="View" color="#82C2F6" width="100px" />
+        </div>
+      </div>
+    </div>
   );
 };
 
