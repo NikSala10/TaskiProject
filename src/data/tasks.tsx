065bfb5b72c166b0f3f19e800f7998241387f008
NikSala10 <salamancanikol10@gmail.com>
2025-09-28 18:18:25 -0500

    feat(components): add new component, AdditionalTasks

--- src/data/tasks.tsx
+++ src/data/tasks.tsx
@@ -1,7 +1,7 @@
 import type { Task } from "../types/TasksType";
 
 export const tasks: Task[] = [
-    { id: '1', name: 'Make the beds', points: 20, time: '10:00 am', priority: 'Medium' },
+    { id: '1', name: 'Make the beds', points: 20, time: '10:00 am', priority: 'Medium', isAdditional: false},
     { id: '2', name: 'Wash the dishes', points: 80, time: '1:00 pm', priority: 'High' },
     { id: '3', name: 'Take out the trash', points: 20, time: '9:00 am', priority: 'Medium' },
     { id: '4', name: 'Sweep the kitchen floor', points: 10, time: '12:00 am', priority: 'Low' },
