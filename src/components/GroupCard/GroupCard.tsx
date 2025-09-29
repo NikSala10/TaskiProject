d950cf2f74c1c2030e404aa4745a2736f347adcc
NikSala10 <salamancanikol10@gmail.com>
2025-09-29 13:13:38 -0500

    fix(pages/create-task): resolve issues in CreateTask page and components

--- src/components/GroupCard/GroupCard.tsx
+++ src/components/GroupCard/GroupCard.tsx
@@ -2,21 +2,25 @@ import type { GroupCardType } from "../../types/GroupCardType";
 import AvatarWithName from "../AvatarWithName/AvatarWithName";
 import Button from "../Button/Button";
 
-const GroupCard = ({ groupName, members }: GroupCardType) => {
+const GroupCard = ({ groupName, members, showRanking = false }: GroupCardType) => {
   return (
     <div className="card-group">
       <h3 className="group-name">{groupName}</h3>
       <div className="members-list">
         {members.map((member, index) => (
-          <AvatarWithName
-            key={index}
-            avatar={member.avatar}
-            namePlayer={member.namePlayer}
-            rol={member.rol}
-          />
+          <div key={index} className="ranking-item">
+            {showRanking && <span className="ranking-number">{index + 1}</span>}
+            <AvatarWithName
+              avatar={member.avatar}
+              namePlayer={member.namePlayer}
+              rol={member.rol}
+              showRanking={showRanking}
+              numPoints={member.numPoints}
+            />
+          </div>
         ))}
       </div>
-        <Button text="Remove" color="#FF935A" width="150px" />
+      {!showRanking && <Button text="Remove" color="#FF935A" width="150px" /> }
     </div>
   );
 };
