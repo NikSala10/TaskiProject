591914a691311ae459ccc55b0e9328d8ede5bd80
yeliani <yelianibarbosa@gmail.com>
2025-09-29 13:27:48 -0500

    feat(create group)Change in css padding

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import CreateGroupPage from "./pages/CreateGroup/CreateGroup";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>}/>
+        <Route path="/" element={<Register/>}/>
         <Route path="/create-group" element={<CreateGroupPage/>}/>
         <Route path="/login" element={<Login />} />
 
