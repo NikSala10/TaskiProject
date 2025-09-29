403359ff424c6efa9e4a3b1bc045f94e1cb694c6
NikSala10 <salamancanikol10@gmail.com>
2025-09-29 13:14:35 -0500

    fix(pages/create-task): resolve issues in CreateTask page and components

--- src/components/AvatarWithName/AvatarWithName.tsx
+++ src/components/AvatarWithName/AvatarWithName.tsx
@@ -1,13 +1,23 @@
 import type { AvatarWithNameProps } from "../../types/GroupCardType";
 
-const AvatarWithName = ({avatar, namePlayer, rol, showRanking}: AvatarWithNameProps) => {
+const AvatarWithName = ({avatar, namePlayer, rol, showRanking, numPoints}: AvatarWithNameProps) => {
 
   return (
     <div className="user-card">
       { showRanking && <div className="number-space"></div>}
         <span className="avatar">{avatar}</span>
+
+      { showRanking && <div className="info-with-ranking">    
+        <span className="name">{namePlayer}</span>
+        <span className="points">{numPoints} Points</span>
+      </div>
+      }
+      { !showRanking && <>    
         <span className="name">{namePlayer}</span>
         <span className="role">{rol}</span>
+      </>
+      }
+        
     </div>
   );
 };
