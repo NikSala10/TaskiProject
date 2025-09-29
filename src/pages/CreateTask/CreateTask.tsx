53d7a9075888f437031d56fd4157f0a42b2d3415
NikSala10 <salamancanikol10@gmail.com>
2025-09-28 20:14:26 -0500

    feat(types): add typings for Input component

--- src/pages/CreateTask/CreateTask.tsx
+++ src/pages/CreateTask/CreateTask.tsx
@@ -1,10 +1,22 @@
+import Button from "../../components/Button/Button";
 import { useSetPageInfo } from "../../hook/UseSetPage";
 
 const CreateTask = () => {
- useSetPageInfo("Create Task");
+  
+  useSetPageInfo("Create Task");
+
+  const sendForm = (e) => {
+      e.preventDefault();
+      alert("task created and assigned");
+  }
 
   return (
-    <p>Create Task</p>
+    <div className="create-task-page">
+      <form onSubmit={(e) => sendForm(e)}>
+            
+          <Button text="Create Task" type="submit" />
+        </form>
+    </div>
   );
 };
 
--- src/types/ButtonType.tsx
+++ src/types/ButtonType.tsx
@@ -1,6 +1,7 @@
 export type ButtonProps = {
   text: string;
   color?: string;    
-  width?: string;     
+  width?: string; 
+  type?: "button" | "submit" | "reset";    
   onClick?: () => void;
 };
