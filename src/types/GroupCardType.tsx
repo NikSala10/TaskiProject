6b5088a4341363d599f1cb5128aa373a1bcd0291
NikSala10 <salamancanikol10@gmail.com>
2025-09-29 09:35:45 -0500

    fix(pages/create-task): resolve issues in CreateTask page and components

--- src/types/GroupCardType.tsx
+++ src/types/GroupCardType.tsx
@@ -4,7 +4,10 @@ export type AvatarWithNameProps = {
   avatar: ReactNode;
   namePlayer: string;
   rol: "Admin"| "Member"; 
+  showRanking?: boolean; 
+  numPoints: number;
 };
+
 
 export type GroupCardType = {
   groupName: string;
