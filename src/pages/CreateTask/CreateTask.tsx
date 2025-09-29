d6d75748eaf1cd942f41e93ec6c72b89a8361da9
NikSala10 <salamancanikol10@gmail.com>
2025-09-28 21:42:48 -0500

    style(page/create task): add styles for createTask page

--- src/pages/CreateTask/CreateTask.tsx
+++ src/pages/CreateTask/CreateTask.tsx
@@ -59,6 +59,12 @@ const CreateTask = () => {
           <Button text="Assign Task" color="#82C2F6" width="480px" />
           </div>
         </form>
+        <div className="heads-up">
+          <h2>Heads Up!</h2>
+          <div className="div-head">
+            <p>Each task comes with points based on its level:</p>
+          </div>
+        </div>
     </div>
   );
 };
