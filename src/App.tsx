313c289ec3f8f9b6b1011ad9318e7fc631674ca1
yeliani <yelianibarbosa@gmail.com>
2025-09-29 15:27:57 -0500

    feat(create group)Change in css margin

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import CreateGroupPage from "./pages/CreateGroup/CreateGroup";
-//import Register from "./pages/Register/Register";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<CreateGroupPage/>}/>
-        <Route path="/create-group" element={<CreateGroupPage/>}/>
+        <Route path="/" element={<Register/>}/>
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/create-group" element={<CreateGroupPage/>}/>
 
       </Routes>
     </Router>
