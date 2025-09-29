2097ac0f9ac79f8b120d0565724ed26a389fed38
NikSala10 <salamancanikol10@gmail.com>
2025-09-28 21:14:33 -0500

    feat(routing): add navigation and route to CreateTask page

--- src/pages/CreateTask/CreateTask.tsx
+++ src/pages/CreateTask/CreateTask.tsx
@@ -14,46 +14,48 @@ const CreateTask = () => {
   return (
     <div className="create-task-page">
       <form>
+        <div className="inputs-all">
+          <InputTask
+          label="Title"
+          placeholder="Ej: Take out the trash"
+          type="text"
+        />
+
+      <InputTask
+          label="Choose group"
+          type="select"
+          options={[
+            { value: "1", label: "Morgan Family" },
+            { value: "2", label: "Chimichangas" },
+          ]}
+        />
         <InputTask
-        label="Title"
-        placeholder="Ej: Take out the trash"
-        type="text"
-      />
+          label="Asignee"
+          type="select"
+          options={[
+            { value: "alta", label: "Choose a member" },
+            { value: "alta", label: "Nuni" },
+            { value: "media", label: "Jui" },
+            { value: "baja", label: "Patiks" },
+          ]}
+        />
 
-    <InputTask
-        label="Choose group"
-        type="select"
-        options={[
-          { value: "1", label: "Morgan Family" },
-          { value: "2", label: "Chimichangas" },
-        ]}
-      />
-      <InputTask
-        label="Asignee"
-        type="select"
-        options={[
-          { value: "alta", label: "Choose a member" },
-          { value: "alta", label: "Nuni" },
-          { value: "media", label: "Jui" },
-          { value: "baja", label: "Patiks" },
-        ]}
-      />
+        <InputTask
+          label="Priority"
+          type="select"
+          options={[
+            { value: "low", label: "Low" },
+            { value: "medium", label: "Medium" },
+            { value: "high", label: "High" },
+          ]}
+        />
 
-      <InputTask
-        label="Priority"
-        type="select"
-        options={[
-          { value: "low", label: "Low" },
-          { value: "medium", label: "Medium" },
-          { value: "high", label: "High" },
-        ]}
-      />
-
-      <InputTask
-      label="Schedule"
-      placeholder="Today, 6:00 PM"
-      type="date"
-      />
+        <InputTask
+        label="Schedule"
+        placeholder="Today, 6:00 PM"
+        type="date"
+        />
+      </div>
 
         <Button text="Assign Task" type="submit" color="#82C2F6" width="400px" />
         </form>
