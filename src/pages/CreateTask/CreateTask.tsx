e12ab6b8680bb8f465541781acb403c53fb0eb91
NikSala10 <salamancanikol10@gmail.com>
2025-09-28 20:23:10 -0500

    feat(types): add typings for Input component

--- src/pages/CreateTask/CreateTask.tsx
+++ src/pages/CreateTask/CreateTask.tsx
@@ -1,4 +1,5 @@
 import Button from "../../components/Button/Button";
+import InputTask from "../../components/InputTask/InputTask";
 import { useSetPageInfo } from "../../hook/UseSetPage";
 
 const CreateTask = () => {
@@ -13,7 +14,10 @@ const CreateTask = () => {
   return (
     <div className="create-task-page">
       <form>
-        
+        <InputTask label="Task Name" placeholder="Enter task name" type="text" />
+        <InputTask label="Description" placeholder="Enter task description" type="text" />
+        <InputTask label="Due Date" placeholder="Select due date" type="date" />
+        <InputTask label="Assign To" placeholder="Enter assignee's name" type="text" />
         <Button text="Assign Task" type="submit" color="#82C2F6" width="400px" />
         </form>
     </div>
--- src/types/InputTask.tsx
+++ src/types/InputTask.tsx
@@ -1,5 +1,11 @@
+export type OptionType = {
+  value: string;
+  label: string;
+};
+
 export type InputProps = {
   label: string;
-  placeholder: string;
-  type?: string; 
-}; 
+  placeholder?: string;
+  type: "text" | "email" | "password" | "number" | "select";
+  options?: OptionType[]; // solo se usa si es select
+};
